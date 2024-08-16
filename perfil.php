3a2bbf4e7364229ce92d3b50d4dcb3eedc399555
nahuel051 <isaias.nahuelramirez@gmail.com>
2024-08-16 12:46:50 -0300

    asdasda

--- perfil.php
+++ perfil.php
@@ -27,7 +27,7 @@ if (is_array($id_usuario_sesion)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil</title>
-    <link rel="stylesheet" href="estilos/estilo.css">
+    <link rel="stylesheet" href="styles/estilo.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
